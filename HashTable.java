6490939350b4be3a1bc5216df12d22b7c9f3ed05
NorikaLiu <31222327+NorikaLiu@users.noreply.github.com>
2017-12-23 06:48:28 -1000

    Add files via upload

--- HashTable.java
+++ HashTable.java
@@ -27,15 +27,15 @@ public class HashTable {
 			return 0;
 		}	
 		else 
-		{int keyInt = Integer.parseInt(key);
-		for (int i = 0; i<keyInt.length(); i++) {
-			int keyValue = 0;
-			keyValue = (keyValue + keyInt [i]) %8;
-			Character.getNumericValue()
+		{int sum = 0;
+			for (int i=0; i < key.length(); i++) {
+			char place = key.charAt(i);
+			sum = sum + place;
 		}
-		
-		}
+		return Math.abs(sum) % 8;
 	}
+	
+}
 	
 	/*
 	 * This method should insert a node containing the given key
@@ -43,6 +43,14 @@ public class HashTable {
 	 * at the head of each linked list for ease of implementation.
 	 */
 	public void insert(String key) {
+		Node a = new Node (key);
+		if (map[hash(key)] != null) {
+			Node temp = new Node(map[hash(key)].record);
+			map[hash(key)].record = key;
+			map[hash(key)].next = temp;
+		} else {
+			map[hash(key)] = a;
+		}
 		// your code here
 	}
 	
@@ -52,6 +60,22 @@ public class HashTable {
 	 */
 	public boolean search(String key) {
 		// your code here
+	int apple = hash (key);
+	Node point = map [apple];
+	int i = 0;
+	while (point != null && i == 0) {
+		if (point.record == map[apple].record)
+			i++;
+			else{
+				point = point.next;
+			}
+		}
+		if( i > 0){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 	
 	/*
@@ -64,6 +88,15 @@ public class HashTable {
 	 * 1: Nicholas Mary Kim Jack
 	 */
 	public void printTable() {
+		Node pointer = map[0];
+		for (int i = 0; i < map.length; i++) {
+			while (pointer != null) {
+				System.out.println(i + ": " + pointer.record);
+				pointer = pointer.next;
+			}
+			pointer = map[i];
+			
+		}
 		// your code here
 	}
 	
@@ -72,6 +105,19 @@ public class HashTable {
 	 */
 	public void delete(String key) {
 		// your code here
+	int num =  hash(key);
+	Node p= map[num];
+	int a = 0;
+	while (p != null && a == 0) {
+		if (p.next.record.equals(key)) {
+			p.next = null;
+			a ++;
+		} else {
+			p = p.next;
+		}
+	
+	}
+	
 	}
 	
 }
